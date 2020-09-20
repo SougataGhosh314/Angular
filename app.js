e9f1f22434170f5378a13db62fdd3c4d91757d2d
SougataGhosh314 <42476034+SougataGhosh314@users.noreply.github.com>
2020-09-20 15:57:14 +0530

    Update app.js
    Removed exposed uri

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://brainstorm:qaz123zxc@cluster0.6hwjp.mongodb.net/products-auth';
+const dbURI = '';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
